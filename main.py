d8b92ac03bc4133cd421a1f772364baba840af7a
OGdB <58260499+OGdB@users.noreply.github.com>
2022-02-14 20:20:26 -0500

    print the map

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-with open("data\jsonmap.json", "r") as fp:
+with open("data\jsonmap2.json", "r") as fp:
     raw_data = json.load(fp)
     if "width" in raw_data:
         map_width = raw_data['width']
@@ -15,13 +15,17 @@ with open("data\jsonmap.json", "r") as fp:
 
     all_tilesets = raw_data['tilesets']
     first_gid = all_tilesets[0]['firstgid']
-    tileset_name = all_tilesets[0]['name']
+    # tileset_name = all_tilesets[0]['name']
 
     # print 1 layer in numbers
     y = 0
     for x in range(map_height):
-        start = x * (map_width - 1)
+        start = x * map_width
         end = start + map_width
         sublist = first_layer_data[start:end]
 
-        print(first_layer_data)
+        ## print out the tiles in ints
+        # s = ""
+        # for v in sublist:
+        #     s += str(v) + " "
+        # print(s)
