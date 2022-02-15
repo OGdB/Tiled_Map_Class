0764679b9e0382b0f40ab9fae03064c32edf8130
OGdB <58260499+OGdB@users.noreply.github.com>
2022-02-14 23:25:15 -0500

    added pygame setup

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import Map
+import pygame
 
+pygame.init()
 
+win_w = 800
+win_h = 600
+win = pygame.display.set_mode((win_w, win_h))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Courier New", 20, True)
+
+# Tile stuff
 def is_tile_rotated(tileset, tile):
     rotated = 0x20000000
     hor_flip = 0x80000000
@@ -28,3 +37,27 @@ for rots in this_map.layers[0][0]:
     string_unrotated += str(is_tile_rotated(this_map.tile_set, rots)) + " / "
 print(string_unrotated)
 
+# UPDATE
+done = False
+while not done:
+    delta_time = clock.tick() / 1000
+
+    # INPUT
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        done = True
+    all_keys = pygame.key.get_pressed()
+    if all_keys[pygame.K_ESCAPE]:
+        done = True
+
+    # DRAWING
+    win.fill((0, 0, 0))
+
+    white = (255, 255, 255)
+    fps_text = "FPS: " + str(round(clock.get_fps()))
+    fps_text_render = font.render(fps_text, True, white)
+    win.blit(fps_text_render, (680, 10))
+
+    pygame.display.flip()
+
+pygame.quit()
