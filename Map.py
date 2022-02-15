4ed8d1e548dcd56b8bd1733cd2695c2fed598512
OGdB <58260499+OGdB@users.noreply.github.com>
2022-02-14 21:25:29 -0500

    tileset attribute

--- Map.py
+++ Map.py
@@ -1,26 +1,24 @@
 import json
 
 class Map:
+    """Class with functions to read map data from json files made with Tiled"""
     def __init__(self, json_path):
-        self.raw_data = ""
+        self.layers = []
+
         with open(json_path, "r") as fp:
-            self.raw_data = json.load(fp)
+            raw_data = json.load(fp)
+            self.tile_set = raw_data['tilesets'][0]
 
-    def get_map(self):
-        """Get the map in the form of multiple lists"""
-        lists = []
-        map_height = self.raw_data['height']
-        map_width = self.raw_data['width']
-        first_layer_data = self.raw_data['layers'][0]['data']
-        for x in range(map_height):
-            start = x * map_width
-            end = start + map_width
-            sublist = first_layer_data[start:end]
-            lists.append(sublist)
+            # Get every layer and store in layers list
+            map_height = raw_data['height']
+            map_width = raw_data['width']
+            for layer in raw_data['layers']:
+                layer_in_lists = []
+                layer_data = layer['data']
+                for x in range(map_height):
+                    start = x * map_width
+                    end = start + map_width
+                    sublist = layer_data[start:end]
+                    layer_in_lists.append(sublist)
 
-        return lists
-            ## print out the tiles in ints
-            # s = ""
-            # for v in sublist:
-            #     s += str(v) + " "
-            # print(s)
+                self.layers.append(layer_in_lists)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import Map
 
 this_map = Map.Map("data\jsonmap2.json")
-for l in this_map.get_map():
-    print(l)
+
+print(this_map.tile_set['name'])
+
+for l in this_map.layers:  # print first layer
+    for t in l:
+        print(t)
+    print("\n")
