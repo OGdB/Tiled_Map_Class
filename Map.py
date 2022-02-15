63aa43112b01558161ffe77e417c82e8fb83e810
OGdB <58260499+OGdB@users.noreply.github.com>
2022-02-14 22:54:32 -0500

    rotation detection

--- Map.py
+++ Map.py
@@ -3,20 +3,19 @@ import json
 class Map:
     """Class with functions to read map data from json files made with Tiled"""
     def __init__(self, json_path):
-        self.layers = []
-
         with open(json_path, "r") as fp:
             raw_data = json.load(fp)
             self.tile_set = raw_data['tilesets'][0]
 
             # Get every layer and store in layers list
+            self.layers = []
             map_height = raw_data['height']
             map_width = raw_data['width']
             for layer in raw_data['layers']:
                 layer_in_lists = []
                 layer_data = layer['data']
-                for x in range(map_height):
-                    start = x * map_width
+                for y in range(map_height):  # get the row for every y-unit
+                    start = y * map_width
                     end = start + map_width
                     sublist = layer_data[start:end]
                     layer_in_lists.append(sublist)
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import Map
 
+
+def is_tile_rotated(tileset, tile):
+    rotated = 0x20000000
+    hor_flip = 0x80000000
+    ver_flip = 0x40000000
+
+    tilecount = tileset["tilecount"]
+    if 0 < tile - rotated < tilecount:
+        return tile - rotated
+    if 0 < tile - hor_flip < tilecount:
+        return tile - hor_flip
+    if 0 < tile - ver_flip < tilecount:
+        return tile - ver_flip
+    if 0 < tile - (hor_flip + rotated) < tilecount:
+        return tile - (hor_flip + rotated)
+    if 0 < tile - (ver_flip + rotated) < tilecount:
+        return tile - (ver_flip + rotated)
+
+    return tile
+
 this_map = Map.Map("data\jsonmap2.json")
 
-print(this_map.tile_set['name'])
+print(this_map.layers[0][0])
+string_unrotated = ""
+for rots in this_map.layers[0][0]:
+    string_unrotated += str(is_tile_rotated(this_map.tile_set, rots)) + " / "
+print(string_unrotated)
 
-for l in this_map.layers:  # print first layer
-    for t in l:
-        print(t)
-    print("\n")
